7e706137f89ff5e9ab66952743d4b4294b62e630
Jacky Ye <jackyy@cqcp.com.tw>
2020-03-17 14:06:31 +0800

    [modify] 隱藏 token

--- app/Console/Commands/majaSlot/Api/Api.php
+++ app/Console/Commands/majaSlot/Api/Api.php
@@ -17,7 +17,7 @@ class Api
 
     public function __construct()
     {
-        self::$header = '0Y8sYtJi55Qeli4Z';
+        self::$header = 'xxx';
         self::$domain = 'https://api.integration.mj-02.com/api/MOGI';
         self::$agentCode = 'jpt';
         self::$agentName = 'jpt';
